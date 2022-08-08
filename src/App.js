dd501217e2aa1ebc26240b3830a0b0e596fb6c43
= <=>
2022-08-08 18:03:51 -0400

    all 4 pages setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-import Landing from "./pages/Landing";
+import { Landing, Error, Register, Dashboard } from "./pages";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
-        <Route path="register" element={<div>Register</div>} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
-        <Route path="*" element={<div>Error</div>} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Register.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Logo, FormRow, Alert } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+import { useAppContext } from "../context/appContext";
+
+// global context and useNavigate later
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: false,
+};
+// if possible prefer local state
+// global state
+
+function Register() {
+  const [values, setValues] = useState(initialState);
+
+  const { isLoading, showAlert, displayAlert } = useAppContext();
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
+    console.log(values);
+  };
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {showAlert && <Alert />}
+        {/* name input */}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+
+        {/* email input */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block" disabled={isLoading}>
+          submit
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
+}
+
+export default Register;
